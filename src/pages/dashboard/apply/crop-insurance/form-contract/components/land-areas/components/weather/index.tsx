b04b7307f8ea216354d76e1e12d05d8da9d26178
tuitstudent <tuitstudent2000@gmail.com>
2025-01-16 15:24:27 +0500

    some changes

--- src/pages/dashboard/apply/crop-insurance/form-contract/components/land-areas/components/weather/index.tsx
+++ src/pages/dashboard/apply/crop-insurance/form-contract/components/land-areas/components/weather/index.tsx
@@ -1,10 +1,18 @@
+// @ts-nocheck
+import React from 'react'
+
 export const Weather = () => {
   return (
-    <iframe
-      width='100%'
-      height='300'
-      src='https://embed.windy.com/embed.html?type=map&location=coordinates&metricRain=default&metricTemp=default&metricWind=default&zoom=5&overlay=wind&product=ecmwf&level=surface&lat=41.261&lon=69.218'
-      frameBorder='0'
-    />
+    <div style={{ width: '100%', height: '100%', position: 'relative' }}>
+      <iframe
+        src='https://embed.windy.com/embed2.html?lat=50.4&lon=14.3&zoom=5&level=surface&overlay=wind&menu=&message=&marker=&calendar=now&pressure=true&type=map&location=coordinates&detail=&detailLat=50.4&detailLon=14.3&metricWind=default&metricTemp=default&radarRange=-1'
+        width='100%'
+        height='300px'
+        frameBorder='0'
+        title='Windy Map'
+        style={{ border: 0 }}
+        allowFullScreen
+      ></iframe>
+    </div>
   )
 }
